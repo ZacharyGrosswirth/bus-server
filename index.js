7e7bff4078e4c60fcd9208a0cf2f9ad76fe5cda6
ZacharyGrosswirth <zacharygrosswirth1@gmail.com>
2025-05-18 00:01:45 -0400

    connection changes

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 
-const port = process.env.PORT || 4000 
+const port = process.env.PORT || 4000;
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://bus-server-zei8.onrender.com",
+];
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      callback(new Error("CORS not allowed"));
+    },
+    methods: ["GET", "POST"],
+  },
+});
 
-app.get('/', (req, res) => {
-  res.send('Ride the Bus server is running...')
-})
+app.get("/", (req, res) => {
+  res.send(`Ride the Bus server is running on port ${port}`);
+});
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
   });
 });
 
 server.listen(port, () => {
-  console.log('server running at http://localhost:3000');
+  console.log(`Server running on port ${port}`);
 });
